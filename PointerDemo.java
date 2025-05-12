10627ed68bf994af63c01cc918e1ecfb0c048944
Manish Ranjan <manishranjan.soft@gmail.com>
2025-05-12 17:08:53 +0530

    pointer code

---
+++ PointerDemo.java
@@ -0,0 +1,13 @@
+public class PointerDemo {
+    public static void main(String[] args) {
+        InnerPointerDemo demo = new InnerPointerDemo();
+        InnerPointerDemo demo2 = demo; // demo2 is a reference to the same object as demo
+        System.out.println(demo2.x);
+        demo2.x = 20;
+        System.out.println(demo.x);
+    }
+}
+class InnerPointerDemo {
+    int x = 10;
+    
+}
