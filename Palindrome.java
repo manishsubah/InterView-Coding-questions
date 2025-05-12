e859a1e8301b15bdae8fd858b4ca04c4a1a36b90
Manish Ranjan <manishranjan.soft@gmail.com>
2025-05-12 19:21:24 +0530

    palindrome code

---
+++ Palindrome.java
@@ -0,0 +1,24 @@
+public class Palindrome {
+    public static boolean isPalindrome(String str) {
+        int left = 0, right = str.length() - 1;
+        System.out.println("Length of the string = " + str.length());
+        System.out.println("left = " + left + ", right = " + right);
+        // Check if the string is empty or has only one character
+        while(left < right) {
+            if(str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String str = "college"; 
+        System.out.println("Is the string a palindrome? = " + isPalindrome(str));
+        str = "madam"; 
+        System.out.println("Is the string a palindrome? = " + isPalindrome(str));
+        str = "cool"; 
+        System.out.println("Is the string a palindrome? = " + isPalindrome(str));
+    }
+}
