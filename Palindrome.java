224912b7ac12c742d843d095a4ead811eb269e05
Manish Ranjan <manishranjan.soft@gmail.com>
2026-02-10 19:19:36 +0530

    update

--- Palindrome.java
+++ Palindrome.java
@@ -3,7 +3,7 @@ public class Palindrome {
         int left = 0, right = str.length() - 1;
         System.out.println("Length of the string = " + str.length());
         System.out.println("left = " + left + ", right = " + right);
-        // Check if the string is empty or has only one characte
+        // Check if the string is empty or has only
         while(left < right) {
             if(str.charAt(left) != str.charAt(right)) {
                 return false;
