a851e4af9f83c408941b697701407c69266b9213
Manish Ranjan <manishranjan.soft@gmail.com>
2026-03-14 20:39:07 +0530

    update

--- ReverseString.java
+++ ReverseString.java
@@ -2,7 +2,7 @@ public class ReverseString {
     public static void main(String[] args) {
         String original = "Hello, World!";
 
-        //using string
+        //using
         StringBuilder reversedBuilder = new StringBuilder(original).reverse();
         System.out.println("Reversed String: " + reversedBuilder);
 
