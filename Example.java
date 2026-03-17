76cf31f4e06ceb4ac13697ca19127c5003c50fa2
Manish Ranjan <manishranjan.soft@gmail.com>
2026-03-17 21:31:40 +0530

    UPDATE

---
+++ Example.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Example {
+public static void main(String[] args) {
+    StringBuilder sb = new StringBuilder("hello");
+    System.out.println("String:-" + sb);
+    sb.append("manish");
+    System.out.println("String:-" + sb);
+}
+}
+
+
