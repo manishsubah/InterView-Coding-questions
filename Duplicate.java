e3470acd0a609a3ee3177e76ebab310f931ea94b
Manish Ranjan <manishranjan.soft@gmail.com>
2026-06-22 21:30:44 +0530

    update

--- Duplicate.java
+++ Duplicate.java
@@ -1,10 +1,10 @@
 public class Duplicate {
-    public static boolean isPalindrome(String str) { //
+    public static boolean isPalindrome(String str) {
     int left = 0;
     int right = str.length() - 1;
 
-    while(left < right){
-        if(str.charAt(left) != str.charAt(right)){
+    while(left < right) {
+        if(str.charAt(left) != str.charAt(right)) {
             return false;
         }
         left++;
